802a338140f71e1992fbfcb9e26720d1d858ea3e
Yuta Shirakawa <yuta1125tp@gmail.com>
2014-03-28 16:46:34 +0000

    initialize repositry

---
+++ bot.py
@@ -0,0 +1,225 @@
+# -*- coding:utf-8 -*-
+
+# twythonを使ったテストコード
+import twython # pythonからtwitterをごちゃごちゃできるライブラリ
+import codecs # 文字コードを指定してファイルの入出力をする場合に使う
+import random # randint使うときに必要
+import sys # 標準出力の文字コードを変更するために用いる
+
+import re # 正規表現のマッチングかける時
+import markov # 文書生成のためのマルコフ課程周りの関数が入ってる。
+
+import pickle # つぶやきログがpickleで保存されてるのを読み込むため
+
+import webapp2
+
+# kawabottpへアクセスするための情報。
+# ちなみに登録時に使用したメールアドレスはyuta1125tp+bot@gmail.com
+consumerKey = 'XR9ImVofpaqa6zqcpeJlgQ'
+consumerSecret = 'm9UAPDO6bFwuPip7kWT4MbJbMi6fY0POTDz92f9zpQ'
+accessToken = '2221327394-qsHDtrC7TgEqATLijM3aM9S4UZyYWeRVjvmyGVw'
+accessSecret = 'vXh3cYMbdKyOqUDeMKfCdBz9g4jn6wy66SNHWnUmjC64i'
+
+def load_dic(load_name):
+    # 言葉を読み込んでくる。
+    f = codecs.open(load_name, 'r', 'utf-8')
+    data = f.read() # .txtの中身全部を読み込んでくる。
+    dictionary = data.split('\n')
+    # for word in dictionary:
+    #     print word
+    # print
+    return dictionary
+
+def save_dic(dictionary, save_name):
+    # 言葉を保存する。
+    f = codecs.open(save_name, 'w', 'utf-8')
+    for word in dictionary:
+        f.write(word+'\n')
+        
+def random_tweet(dictionary):
+    # 保持している辞書からランダムに選んで表示する
+    for i in xrange(10):
+        print dictionary[random.randint(0,len(dictionary)-1)]
+
+def remove_kagi_account(api, ids, name):
+    # 鍵付きアカウントの内容をツイートしてしまってはまずいので
+    # 鍵付きｱカウントのIDを削除する
+    index = []
+    list = api.get_followers_list(screen_name = name)
+    for i in xrange(len(list[u'users'])):
+        if list[u'users'][i][u'protected']:
+            ids.remove(list[u'users'][i][u'id'])
+            
+def get_samples(api, name='kawabottp'):
+    # 指定したユーザーのフォロワーの最近のツイートを取得してtxtファイルに保存
+    # ユーザをキーに辞書形式で保存する。無駄かもしれないけど…
+    
+    # fout = codecs.open('samples.txt', 'w', 'utf-8')
+    
+    ids = get_friends_id(api, name)
+    remove_kagi_account(api, ids, name)
+    
+    num_tweet = 20
+    
+    tweet_dict = {}
+    for i in xrange(len(ids)):
+        tweet_dict[ids[i]]=[] # ユーザIDをキーに1ユーザにつき1つのリストにツイートを保存
+        try:
+            user_timeline = api.get_user_timeline(user_id = ids[i], 
+                                                  count = num_tweet)
+        except Exception as e:
+            print e
+        for j in xrange(len(user_timeline)):
+            tweet_dict[ids[i]].append(user_timeline[j]['text'])
+
+
+    # pbar.finish()
+    return tweet_dict
+    
+def remove_at_tweet(tweet_dict):
+    # 知らない人への@ツイートや#のハッシュタグをしてしまわないように
+    index = []
+    for follower_id in tweet_dict.keys():
+        for i in xrange(len(tweet_dict[follower_id])):
+            if bool(re.search(u'.*@.*', tweet_dict[follower_id][i])):
+                index.append(i)
+            elif bool(re.search(u'.*#.*', tweet_dict[follower_id][i])):
+                index.append(i)
+        for j in reversed(index):
+            tweet_dict[follower_id].pop(j)
+    
+def remove_url_tweet(tweet_dict):
+    # URLに繋がりそうなツイートは除く
+    index = []
+    url_keywords = [
+        u'goo', u'pic.', u'.com', u'http', u'.ly', u',me']
+    for follower_id in tweet_dict.keys():
+        for i in xrange(len(tweet_dict[follower_id])):
+            for key_word in url_keywords:
+                if bool(re.search(u'.*' + key_word + u'.*', 
+                                  tweet_dict[follower_id][i])):
+                    index.append(i)
+                    break
+        for j in reversed(index):
+            tweet_dict[follower_id].pop(j)
+
+def remove_retweet(tweet_dict):
+    # 引用やリツイートしないようにする
+    # RT, QT
+    # これを除く。
+    index = []
+    for follower_id in tweet_dict.keys():
+        for i in xrange(len(tweet_dict[follower_id])):
+            if bool(re.search(u'.*RT.*', tweet_dict[follower_id][i])):
+                index.append(i)
+            elif bool(re.search(u'.*QT.*', tweet_dict[follower_id][i])):
+                index.append(i)
+        for j in reversed(index):
+            tweet_dict[follower_id].pop(j)
+    
+def get_friends_id(api, name):
+    # 指定したユーザー"が"フォローしているユーザーのID一覧を返す
+    try:
+        users_friends_list = api.get_friends_ids(screen_name=name)
+    except Exception as e:
+        print e
+    # print type(users_friends_list) # dict形式
+    # print len(users_friends_list) # 要素が5つ
+    # print users_friends_list['ids'] # followしているユーザのidのリストを返す 
+    return users_friends_list['ids']
+    
+def get_follwers_id(api, name):
+    # 指定したユーザー"を"フォローしているユーザーのID一覧を返す
+    try:
+        users_followers_list = api.get_followers_ids(screen_name=name)
+    except Exception as e:
+        print e
+    # print type(users_friends_list) # dict形式
+    # print len(users_friends_list) # 要素が5つ
+    # print users_friends_list['ids'] # followしているユーザのidのリストを返す 
+    return users_followers_list['ids']
+    
+def tweet_msg():
+    
+    #dictionary = load_dic('dictionary.txt')
+    #save_dic(dictionary, 'dictionary2.txt')
+    #random_tweet(dictionary)
+    
+    api = twython.Twython(app_key=consumerKey,
+                  app_secret=consumerSecret,
+                  oauth_token=accessToken,
+                  oauth_token_secret=accessSecret)
+    
+    tweet_dict = get_samples(api)
+    
+    remove_at_tweet(tweet_dict)
+    remove_url_tweet(tweet_dict)
+    remove_retweet(tweet_dict)
+    # 辞書として持ってても仕方ないので辞書の解体
+    # 全部合わせて1つのUnicode型変数にする。
+    # 全部合わせて1つにするとURLエンコードした時にGAEから開けるURLの上限の長さを超えてしまう。
+    # 1ツイート毎別々にYahooに問い合わせる方針に転換。
+    tweet_unicode = u''
+    tweet_unicode_list = [] # 1つのつぶやきが1つの要素なリスト
+    for i in tweet_dict.keys(): # keys
+        for j in xrange(len(tweet_dict[i])):
+            tweet_unicode_list.append(tweet_dict[i][j])
+            tweet_unicode += tweet_dict[i][j]
+
+    f = open('tweet_log.pkl')
+    l = pickle.load(f)
+    f.close()
+
+    tweet_unicode_list=tweet_unicode_list+l
+    
+    # print tweet_unicode
+    # src = codecs.open("samples.txt", 'r', 'utf-8').read()
+
+    import markov
+    wordlist=[]
+    # いっぺんに投げるとyahooに投げるURLが長くなりすぎてダメなので1ツイートごと細かく区切る
+    for tweet in tweet_unicode_list:
+        # print tweet
+        wordlist=wordlist+markov.wakati_MeCab([tweet])
+        # wordlist=wordlist+markov.wakati_Yahoo([tweet])
+    """
+    wordlist = markov.wakati_Yahoo(tweet_unicode_list) # ここで元となる文章が分かち書きに変換される
+    """
+    """
+    f = open('wordlist.pkl','w')
+    pickle.dump(wordlist,f)
+    f.close()
+    del wordlist
+    
+    f = open('wordlist.pkl','r')
+    wordlist = pickle.load(f)
+    f.close()   
+    """
+    mc_table = markov.make_MC_table2(wordlist)
+    
+    sentence = markov.generate_sentence2(mc_table, wordlist)
+    sentence_list = re.split(u'。', sentence)
+    tweet = u""
+    tweet_index=0
+    while(1):
+        if sentence_list[tweet_index]!=u'':
+            break
+        tweet_index += 1
+    
+    try:
+        api.update_status(status=sentence_list[tweet_index])
+    except twython.TwythonError as e:
+        print e    
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self): 
+        tweet_msg()
+        self.response.write('Hello world!')
+ 
+app = webapp2.WSGIApplication([
+    ('/.*', MainHandler)
+], debug=True)
+
+if __name__=="__main__":
+    tweet_msg()
+
---
+++ markov.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+import random
+# import MeCab
+import codecs
+
+import re
+
+import urllib
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+import sys
+# sys.stdin  = codecs.getreader('utf-8')(sys.stdin)
+# sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
+appid = "dj0zaiZpPWNQYUtGT0phWWV6bCZzPWNvbnN1bWVyc2VjcmV0Jng9ODU-"  # 登録したアプリケーションID
+pageurl = "http://jlp.yahooapis.jp/MAService/V1/parse"
+
+"""
+def wakati(text):
+    t = MeCab.Tagger("-Owakati")
+    m = t.parse(text.encode('utf-8'))
+    # parseで分かち書きにされるのはスペースで区切られたstr
+    # print 'print m'
+    # print m #.encode('utf-8')
+    # Unix系ならrstripするのは\nだけでいいけど、Windowsは\r\nを除いてあげなきゃいけない
+    result = m.rstrip(" \r\n").split(" ")
+    # そのstrからスペース+改行文字を除き、スペース毎に分割したものがlist構造で格納されてるresult
+    # MeCabで処理した結果は文字列型なのでUnicode型にデコードしてやる
+    for i in xrange(len(result)):
+        result[i] = unicode(result[i], 'utf-8', 'ignore')    
+    return result
+"""
+
+def wakati_Yahoo(sentence, appid=appid, results="ma", filter="1|2|3|4|5|6|7|8|9|10|11|12|13"):
+    # Yahooのサービスを利用
+    # 改行文字が除かれていないので注意。
+    if isinstance(sentence, list):
+        # 文章をURLエンコーディング
+        result = []
+        for i in xrange(len(sentence)):
+            # print i
+            sentence_encoded = urllib.quote_plus(sentence[i].encode("utf-8"))
+            query = u"%s?appid=%s&results=%s&filter=%s&sentence=%s" % (pageurl, appid, results, filter, sentence_encoded)
+            c = urllib2.urlopen(query)
+            soup = BeautifulSoup(c.read())
+            """
+            return [(w.surface.string, w.reading.string, w.pos.string)
+                    for w in soup.ma_result.word_list]
+            """
+            result_tuple = [(w.surface.string, w.reading.string, w.pos.string)
+                      for w in soup.ma_result.word_list]
+            for i in xrange(len(result_tuple)):
+                # 長さ3のタプルだけど、こんな形式なので1つ目のみを使う(大学、だいがく、名詞)
+                result.append(result_tuple[i][0])
+        return result        
+    elif instance(sentence, unicode): # 引数がUnicode
+        # 文章をURLエンコーディング
+        sentence = urllib.quote_plus(sentence.encode("utf-8"))
+        query = u"%s?appid=%s&results=%s&filter=%s&sentence=%s" % (pageurl, appid, results, filter, sentence)
+        c = urllib2.urlopen(query)
+        soup = BeautifulSoup(c.read())
+        """
+        return [(w.surface.string, w.reading.string, w.pos.string)
+                for w in soup.ma_result.word_list]
+        """
+        result_tuple = [(w.surface.string, w.reading.string, w.pos.string)
+                  for w in soup.ma_result.word_list]
+        result = []
+        for i in xrange(len(result_tuple)):
+            # 長さ3のタプルだけど、こんな形式なので1つ目のみを使う(大学、だいがく、名詞)
+            result.append(result_tuple[i][0])   
+        return result
+    else:
+        print 'sentence is invalid!'
+        exit()
+def make_MC_table2(wordlist):
+    # Create table of Markov Chain
+    markov = {}
+    w1 = u""
+    w2 = u""
+    # wordlistにどんどん事例を保存していく。
+    # 事例: (w1, w2) -> w3
+    # 直前の2単語？をキーに次につづく単語を予測
+    for word in wordlist:
+        if w1 and w2:
+            if (w1, w2) not in markov:
+                markov[(w1, w2)] = [] # 初めて見る単語の連続だった場合はキーに登録
+            markov[(w1, w2)].append(word) # そのキーの後に単語を登録(リスト)
+        w1, w2 = w2, word # 次の単語に写る
+    return markov
+    
+def make_MC_table1(wordlist):
+    # Create table of Markov Chain
+    markov = {}
+    w1 = u""
+    # wordlistにどんどん事例を保存していく。
+    # 事例: (w1) -> w3
+    # 直前の1単語？をキーに次につづく単語を予測
+    for word in wordlist:
+        if w1:
+            if w1 not in markov:
+                markov[w1] = [] # 初めて見る単語の連続だった場合はキーに登録
+            markov[w1].append(word) # そのキーの後に単語を登録(リスト)
+        w1 = word # 次の単語に写る
+    return markov
+    
+def generate_sentence2(MCtable, wordlist):
+    # Generate Sentence
+    # 前の単語1つに注目
+    count = 0
+    sentence = u""
+    # 生成する文章のはじめの2単語はランダムに選ぶ 
+    w1, w2  = random.choice(MCtable.keys())
+    sentence += w1
+    sentence += w2
+    while count < len(wordlist):
+        # 直前の2単語をもとにその後ろにくる単語を事例ベースで予測。
+        # 事例が複数ある場合はランダムに選ぶ
+
+        # w1,w2に存在しないキーの組み合わせが起きるとエラーで落ちる。
+        # 最後のツイートの後に言葉が続かないので、キーがなくなる。
+        # キーがない場合はもう一度キーを取り直す。
+        # 学習用の文章が十分多かったらそんなことなくなるんだろうけど。。。
+        if not MCtable.has_key((w1, w2)):
+            w1, w2  = random.choice(MCtable.keys())
+        tmp = random.choice(MCtable[(w1, w2)])
+        # print tmp
+        sentence += tmp
+        w1, w2 = w2, tmp
+        count += 1
+    return sentence
+    
+def generate_sentence1(MCtable, wordlist):
+    # Generate Sentence
+    count = 0
+    sentence = u""
+    # 生成する文章のはじめの1単語はランダムに選ぶ 
+    w1 = random.choice(MCtable.keys())
+    while count < len(wordlist):
+        # 直前の2単語をもとにその後ろにくる単語を事例ベースで予測。
+        # 事例が複数ある場合はランダムに選ぶ
+        tmp = random.choice(MCtable[w1])
+        # print tmp
+        sentence += tmp
+        w1 = tmp
+        count += 1
+    return sentence
+ 
+if __name__ == "__main__":
+    # fout = codecs.open('out.txt', 'w', 'utf-8')
+    fout = open('out.txt', 'w')
+    filename = "samples.txt"
+    src = codecs.open(filename, 'r', 'utf-8').read()
+    # src = open(filename, 'r').read()
+    print src# .decode('utf-8')
+
+    wordlist = wakati(src) # ここで元となる文章が分かち書きに変換される
+    
+    markov = make_MC_table1(wordlist)
+    
+    sentence = generate_sentence1(markov, wordlist)
+    
+    tmp = re.split(u'。', sentence)
+    print tmp[0]
+    # 保存する際はUnicode型から文字列型にエンコードする
+    fout.write(sentence.encode('utf-8'))
+    # print sentence.encode('utf-8')
