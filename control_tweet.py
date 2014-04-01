cbdf638fd7f225d724ecd72a2055488063c7790f
Yuta Shirakawa <yuta1125tp@gmail.com>
2014-04-01 23:00:04 +0900

    coment out

--- control_tweet.py
+++ control_tweet.py
@@ -15,16 +15,12 @@ def multiple_punctuate(sentence):
     
 def punctuate_control(sentence_list):
     # 生成されたつぶやき候補がUnicode型でリストで入ってる
-    print "punctuate_mark"
     return_list = []
     for sentence in list(sentence_list):
-        print sentence
         sentence = shorten_punctuate(sentence)
         sentence = multiple_punctuate(sentence)
-        print sentence
-        print type(sentence)
-        if bool(re.search(u'.*。$', sentence)):
-            print u"語尾が。"
+        #if bool(re.search(u'.*。$', sentence)):
+        #    print u"語尾が。"
         return_list.append(sentence)
 
     return return_list
