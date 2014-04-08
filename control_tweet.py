a26314e14d36beb7f939857fa9c568276b57dde0
Yuta Shirakawa <yuta1125tp@gmail.com>
2014-04-08 23:47:59 +0900

    add some function for bracket. but n

--- control_tweet.py
+++ control_tweet.py
@@ -1,7 +1,111 @@
 # -*-coding:utf-8 -*-
 # 自動生成したつぶやきを後からいじくる
 import re
+"""
+def bracket_control(sentence):
+    sentence = only_left_bracket(sentence)
+    sentence = only_right_bracket(sentence)
+    return sentence
+    
+def only_left_bracket(sentence):
+    # 対応する右括弧を文末につける。
+    #(
+    #{
+    #[
+    #（
+    #｛
+    #「
+    bracket_stack=[]
+    for uni in sentence:
+        if bool(re.search(u'（', uni)):
+            bracket_stack.append(u'）')
+        if bool(re.search(u'｛', uni)):
+            bracket_stack.append(u'｝')
+        if bool(re.search(u'「', uni)):
+            bracket_stack.append(u'」')
+        if bool(re.search(u'\(', uni)):
+            bracket_stack.append(u')')
+        if bool(re.search(u'{', uni)):
+            bracket_stack.append(u'}')
+        if bool(re.search(u'\[', uni)):
+            bracket_stack.append(u']')
 
+        #if bool(re.search(u'[）｝」\)}\]]', uni)):
+        #    bracket_stack.pop()
+        
+        if bool(re.search(u'）', uni)):
+            if u'）' in bracket_stack:
+                bracket_stack.pop(bracket_stack.index(u'）'))
+        if bool(re.search(u'｝', uni)):
+            if u'｝' in bracket_stack:
+                bracket_stack.pop(bracket_stack.index(u'｝'))
+        if bool(re.search(u'」', uni)):
+            if u'」' in bracket_stack:
+                bracket_stack.pop(bracket_stack.index(u'」'))
+        if bool(re.search(u'\)', uni)):
+            if u')' in bracket_stack:
+                bracket_stack.pop(bracket_stack.index(u')'))
+        if bool(re.search(u'}', uni)):
+            if u'}' in bracket_stack:
+                bracket_stack.pop(bracket_stack.index(u'}'))
+        if bool(re.search(u'\]', uni)):
+            if u']' in bracket_stack:
+                bracket_stack.pop(bracket_stack.index(u']'))
+        
+    bracket_stack.reverse()
+    for bracket in bracket_stack:
+        sentence += bracket
+    return sentence
+    
+def only_right_bracket(sentence):
+    # 対応する左括弧を文末につける。
+    bracket_stack=[]
+    # 逆順で見る
+    print sentence
+    rev_sentence = list(sentence)
+    print rev_sentence
+    rev_sentence.reverse()
+    print rev_sentence
+    for uni in rev_sentence:
+        if bool(re.search(u'）', uni)):
+            bracket_stack.append(u'（')
+        if bool(re.search(u'｝', uni)):
+            bracket_stack.append(u'｛')
+        if bool(re.search(u'」', uni)):
+            bracket_stack.append(u'「')
+        if bool(re.search(u'\)', uni)):
+            bracket_stack.append(u'(')
+        if bool(re.search(u'}', uni)):
+            bracket_stack.append(u'{')
+        if bool(re.search(u'\]', uni)):
+            bracket_stack.append(u'[')
+
+        if bool(re.search(u'[（｛「\({\[]]', uni)):
+            bracket_stack.pop()
+        
+        #if bool(re.search(u'）', uni)):
+        #    bracket_stack.pop()
+        #if bool(re.search(u'｝', uni)):
+        #    bracket_stack.pop()
+        #if bool(re.search(u'」', uni)):
+        #    bracket_stack.pop()
+        #if bool(re.search(u'\)', uni)):
+        #    bracket_stack.pop()
+        #if bool(re.search(u'}', uni)):
+        #    bracket_stack.pop()
+        #if bool(re.search(u'\]', uni)):
+        #    bracket_stack.pop()
+        
+    bracket_stack.reverse()
+    
+    # 逆順にしていたのを元に戻す
+    #sentence.reverse()
+
+    for bracket in bracket_stack:
+        sentence = bracket + sentence
+    return sentence
+"""
+    
 def convert_dot2maru(sentence):
     # 1つのツイート中は。、か．，か統一する。
     sentence = re.sub(u"．",u"。", sentence)
@@ -25,6 +129,10 @@ def multiple_punctuate(sentence):
         sentence = sentence[0:-1]
     return sentence
     
+def add_punctuate(sentence):
+    # 、が文中にある場合は文末に。を加える。
+    pass
+    
 def punctuate_control(sentence_list):
     # 生成されたつぶやき候補がUnicode型でリストで入ってる
     return_list = []
@@ -37,3 +145,9 @@ def punctuate_control(sentence_list):
         return_list.append(sentence)
 
     return return_list
+"""    
+if __name__=="__main__":
+    uni = u"｛((「「｛私の名前は白川悠太」"
+    print uni
+    print bracket_control(uni)
+"""
